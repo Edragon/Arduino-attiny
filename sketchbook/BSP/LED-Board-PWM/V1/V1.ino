4bbd59b60517d3a7be07065500bbb9d42be1a296
Electrodragon <info@electrodragon.com>
2021-09-03 15:26:25 +0800

    update

---
+++ sketchbook/BSP/LED-Board-PWM/V1/V1.ino
@@ -0,0 +1,25 @@
+#define LED 4
+#define IR 0
+#define GND_detect 3
+
+// attiny 202
+// use internal clock 8MHZ
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(IR, INPUT);
+  pinMode(GND_detect, INPUT);
+}
+
+
+void loop() {
+  
+  if (digitalRead (GND_detect) == 0 ) {
+    digitalWrite(LED, HIGH);
+    delay(2000);
+
+    digitalWrite(LED, LOW);
+    delay(2000);
+  }
+  
+}
